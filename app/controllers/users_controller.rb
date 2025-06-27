b544db8088364f40272d1315b852cf883936ebc1
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2025-06-28 01:10:52 +0545

    Add users controller and update navbar

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
+  def show
+    @location_tags = @user.location_tags
+    @profession_tags = @user.profession_tags
+    @is_own_profile = (@user == current_user)
+    @offerings = @user.offerings.order(created_at: :desc)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
