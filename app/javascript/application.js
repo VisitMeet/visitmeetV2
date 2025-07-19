345d4a8ae3121c135ec403839ba5594ae69eda99
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2025-07-19 23:02:22 +0545

    fix: Use button_to for booking actions
    This commit fixes a routing error that occurred when accepting bookings. The `link_to` helpers were sending `GET` requests instead of `PATCH` requests, causing the error.
    
    The `link_to` helpers have been replaced with `button_to` for all booking actions (accept, decline, cancel) to ensure that the correct `PATCH` requests are sent.
    
    Additionally, `@rails/ujs` has been added to the project to provide unobtrusive JavaScript functionality, and the `application.js` file has been updated to import and start it.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
+import "@rails/ujs"
 
 import "jquery"
 window.$ = window.jQuery = jQuery
 
 import "autosuggestion"
 import "autocomplete"
+
+Rails.start()
