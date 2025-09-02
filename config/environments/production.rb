ca1670b1155f09c48b8ed913ddf5e8a59421a5ae
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2025-09-02 14:58:01 +0545

    Fix importmap asset loading

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
 
+  # Compile assets at runtime when precompiled files are missing.
+  config.assets.compile = true
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 pin "application", to: "application.js", preload: true
-pin "@hotwired/turbo-rails", to: "/assets/turbo.min-3c33ba92ac879121a67767bb3b0675260e3cc1fd05c61403e2c63a26bbc1ed9c.js", preload: true
-pin "@hotwired/stimulus", to: "/assets/stimulus.min-82ab9b5ab642258a9e0b2c0e6acb984cab3360a464465cd4e9996b636efea9e4.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.3.0/app/javascript/turbo/index.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js", preload: true
+pin "@hotwired/stimulus-loading", to: "https://ga.jspm.io/npm:@hotwired/stimulus-loading@1.2.2/dist/stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 pin_all_from "app/javascript/channels", under: "channels", preload: true
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.0.min.js", preload: true
