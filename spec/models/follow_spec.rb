fdf9967878225cbe327ca54c8a74c8a1ed51748f
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2025-09-04 20:45:07 +0545

    Prevent self-follow

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Follow, type: :model do
   describe "validations" do
     subject { create(:follow) }
     it { should validate_uniqueness_of(:follower_id).scoped_to(:followed_id) }
+
+    it "does not allow a user to follow themselves" do
+      user = create(:user)
+      follow = build(:follow, follower: user, followed: user)
+
+      expect(follow).not_to be_valid
+      expect(follow.errors[:followed_id]).to include("can't be the same as follower")
+    end
   end
 end
